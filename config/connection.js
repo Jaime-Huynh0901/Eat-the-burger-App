e3231f241b2e3d39b891983b27feea35e4312abe
Jaime Huynh <taihuynh0901@gmail.com>
2020-07-08 00:38:07 -0700

    Heroku Deploy

--- config/connection.js
+++ config/connection.js
@@ -5,11 +5,11 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        host: "localhost",
+        host: "nnmeqdrilkem9ked.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
         port: 3306,
-        user: "root",
-        password: "Enter your own password",
-        database: "burgers_db"
+        user: "mm703opwiw14nojh",
+        password: "ojw91pxht3buflq3",
+        database: "fgwmhsp1n14ipobw"
     });
 };
 
